63d1fa28e58085f99b314824fc885ac87a951b47
JohannaPeanut <Johanna.k.michel@posteo.de>
2022-04-03 20:45:02 +0200

    API working

--- server.js
+++ server.js
@@ -3,56 +3,59 @@ const axios = require('axios');
 
 const app = express();
 
-app.locals.pageTitle = 'Asís María';
+const openaiUrl= 'https://api.openai.com/v1/engines';
+const API_KEY = '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';
+const ORG_ID = 'org-P533oUUjUCTr4feSPvlg7VKW';
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.locals.pageTitle = 'How to...';
 
 app.use(express.static('public'));
 
-  app.get('/', (request, response) => {
-    response.sendFile(__dirname + '/views/index.html');
+app.get('/', (request, response) => {
+    response.render('index');
   });
 
-//const answer = require('./api-fetch.js');
-
-
-const openaiUrl= 'https://api.openai.com/v1/engines';
-const API_KEY = '';
-const ORG_ID = 'org-P533oUUjUCTr4feSPvlg7VKW';
-const inputText = "Some horses like to be outside";
-  
-
-const answer = (input) => {
+app.get('/results', (req, res) => {
+  //res.render('results')
+  const term = "How to " + req.query.term;
+  const urlHowToAI = 'http://3.68.197.29:8000/answers?user_prompt=' + term.replace(/ /g, "%20") + '&length=300';
   axios({
-      url: openaiUrl,
-      method: 'get',
-      headers: {'Authorization': 'Bearer ' + API_KEY,
-      'OpenAI-Organization': ORG_ID},
-      withCredentials: true, // default is false
-      responseType: 'json', // default
-      responseEncoding: 'utf8', // default
-    })
-  
-  axios({
-    method: 'post',
-    url: 'https://api.openai.com/v1/engines/text-davinci-002/completions',
-    headers: {
-      'Authorization': 'Bearer ' + API_KEY,
-      'Content-Type': 'application/json'},
-    data : {
-      prompt: input,
-      temperature: 0,
-      max_tokens: 6
-    },
+    url: urlHowToAI,
+    method: 'get',
+    headers: {'accept': 'application/json'},
   })
-  .then(function (response) {
-    console.log('----- REQUEST -----' + response)
-    console.log(response.status)
-    console.log(response.statusText)
-    console.log(input + response.data.choices[0].text)
-    return response.data.choices[0].text;
-  });
-  
+.then(function (response) {
+  console.log('----- REQUEST -----' + response)
+  console.log(response.status)
+  console.log(response.statusText)
+  console.log(response.data)
+  res.render('results', { result : { resultAPI : response.data, question : req.query.term}})
+});
+});
+
+
+
+
+
+
+function testHowToAI(term) {
+  const urlHowToAI = 'http://3.68.197.29:8000/answers?user_prompt=' + term.replace(/ /g, "%20") + '&length=300';
+  axios({
+    url: urlHowToAI,
+    method: 'get',
+    headers: {'accept': 'application/json'},
+  })
+.then(function (response) {
+  console.log('----- REQUEST -----' + response)
+  console.log(response.status)
+  console.log(response.statusText)
+  console.log(response.data)
+});
 }
 
-answer(inputText)
+testHowToAI("make bread");
 
 app.listen(process.env.PORT || 8080, () => console.log('All OK'));
